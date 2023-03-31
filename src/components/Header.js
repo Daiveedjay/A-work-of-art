a9f3c605fe6c06a9bb4f9c35ad1d7225f4a77c90
Daiveed <jajadavidjid@gmail.com>
2023-03-31 20:23:40 +0100

    Cleaning the codebase by removing unused imports

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,62 +1,8 @@
-// import "./Header.css";
-
-// // import Logo from "../assests/logo.svg";
-// import Logo from "../media/logo.svg";
-// import { Link } from "react-router-dom";
-
-// import { motion } from "framer-motion";
-// import Music from "./Music";
-// export default function Header() {
-//   return (
-//     <motion.nav animate={{}} className="header__nav">
-//       <Link to="/">
-//         <motion.img
-//           drag
-//           dragConstraints={{
-//             top: 0,
-//             left: 0,
-//             right: 0,
-//             bottom: 0,
-//           }}
-//           src={Logo}
-//           alt="Logo svg"
-//         />
-//       </Link>
-//       {/* <Music /> */}
-//       <motion.p
-//         whileHover={{
-//           color: "blue",
-//         }}
-//         transition={{
-//           type: "spring",
-//           stiffness: 120,
-//         }}
-//         className="header-text primary-text"
-//       >
-//         start slideshow
-//       </motion.p>
-//       <motion.p
-//         whileHover={{
-//           color: "blue",
-//         }}
-//         transition={{
-//           type: "spring",
-//           stiffness: 120,
-//         }}
-//         className="header-text primary-text"
-//       >
-//         stop slideshow
-//       </motion.p>
-//     </motion.nav>
-//   );
-// }
-
 import { useEffect, useState } from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Header.css";
 import Logo from "../media/logo.svg";
 import Music from "./Music";
-import { motion } from "framer-motion";
 
 export default function Header() {
   const [isSlideshowActive, setIsSlideshowActive] = useState(false);
@@ -84,7 +30,7 @@ export default function Header() {
   };
 
   return (
-    <motion.nav animate={{}} className="header__nav">
+    <nav className="header__nav">
       <Link to="/">
         <img className="logo" src={Logo} alt="Logo svg" />
       </Link>
@@ -98,6 +44,6 @@ export default function Header() {
           start slideshow
         </p>
       )}
-    </motion.nav>
+    </nav>
   );
 }
